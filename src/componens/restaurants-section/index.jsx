98c56e0fe02a38e404a7dd06267161322d0fa7f9
pershinigor <pershinigor111@gmail.com>
2025-01-22 13:10:04 +0200

    added restaurant data

--- src/componens/restaurants-section/index.jsx
+++ src/componens/restaurants-section/index.jsx
@@ -4,15 +4,13 @@ import RestaurantsCard from "../restaurants-card/index.jsx";
 const RestaurantsSection = () => {
   return (
     <div className="restaurants-section">
-      <div className="container-fluid">
-        <div className="restaurants-section__main">
-          <HeadingSection
-            className="restaurants-section__heading"
-            headingText="Restaurants"
-          />
-          <div className="restaurants-section__cards">
-            <RestaurantsCard className="restaurants-section__card" />
-          </div>
+      <div className="restaurants-section__main">
+        <HeadingSection
+          className="restaurants-section__heading"
+          headingText="Restaurants"
+        />
+        <div className="restaurants-section__cards">
+          <RestaurantsCard className="restaurants-section__card" />
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-// import { restaurants } from "./constants/mock.js";
 import "../assets/styles/index.css";
 import RestaurantsSection from "./componens/restaurants-section/index.jsx";
 
@@ -9,13 +8,6 @@ const reactRoot = createRoot(root);
 
 reactRoot.render(
   <>
-    {/*<ul className="list">*/}
-    {/*  {[1, 2, 3, 4, 5, 6, 7].map((item) => (*/}
-    {/*    <li className="item" key={item}>*/}
-    {/*      {item}*/}
-    {/*    </li>*/}
-    {/*  ))}*/}
-    {/*</ul>*/}
     <RestaurantsSection />
   </>,
 );
